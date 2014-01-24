a0301c787fdec5f47afd0c3d6717b608d11981ad
klaemo <clemens.stolle@gmail.com>
2014-01-24 19:34:44 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,101 @@
+var archiver = require('archiver')
+var knox = require('knox')
+var fs = require('fs')
+var path = require('path')
+var moment = require('moment')
+
+module.exports = function backup (opts, done) {
+  var s3 = knox.createClient(opts.s3)
+  
+  backup.pack(opts, function (err, path, bytes) {
+    if (err) return done(err)
+    
+    backup.upload(s3, path, bytes, function (err, res) {
+      if (err) return done(err)
+    
+      backup.clean(s3, opts.retention, function (err, data) {
+        if (err) return done(err)
+        done()
+      })
+    })
+  })
+}
+
+module.exports.pack = pack
+module.exports.upload = upload
+module.exports.clean = clean
+
+function pack (opts, cb) {
+  var output = fs.createWriteStream(opts.dest)
+  var archive = archiver(opts.format || 'zip')
+
+  output.on('close', function () {
+    cb(null, output.path, output.bytesWritten)
+  })
+
+  archive.on('error', onError)
+  output.on('error', onError)
+
+  function onError(err) { throw err }
+
+  archive.pipe(output)
+
+  archive.bulk([
+    { expand: true, cwd: opts.cwd, src: opts.src }
+  ])
+
+  archive.finalize(function (err, bytes) {
+    if (err) return onError(err)
+  })
+}
+
+function upload (s3, archive, bytes, cb) {
+  var fileName = path.basename(archive)
+  var req = s3.put(fileName, {
+    'Content-Length': bytes,
+    'Content-Type': 'application/zip'
+  })
+
+  fs.createReadStream(archive).pipe(req)
+
+  req.on('response', function (res) {
+    del(archive, function () {
+      cb(null, res)
+    })
+  })
+
+  req.on('error', function (err) {
+    del(archive, function () {
+      cb(err)
+    })
+  })
+}
+
+function del (file, cb) {
+  fs.unlink(file, function (err) {
+    if (err) return console.error(err)
+    cb()
+  })
+}
+
+function clean (s3, ret, cb) {
+  s3.list(function (err, data) {
+    if (err) return cb(err)
+
+    var retention = moment().subtract(ret.match(/\D+/)[0], ret.match(/^\d+/)[0])
+
+    var old = data.Contents.filter(function (file) {
+      return moment(file.LastModified).isBefore(retention)
+    }).map(function (file) {
+      return file.Key
+    })
+
+    if (!old.length) return cb(null, [])
+
+    s3.deleteMultiple(old, function (err, res) {
+      if (err) return cb(err)
+      cb(null, old)
+    })
+  })
+}
+
---
+++ test/index.js
@@ -0,0 +1,41 @@
+var backup = require('../')
+var assert = require('assert')
+var fs = require('fs')
+var knox = require('knox')
+
+var i = Math.round(Math.random() * 100)
+var dest = __dirname + '/test' + i + '.zip'
+var conf = require('../../s3.json')
+conf.bucket = 'backuper-test'
+var s3 = knox.createClient(conf)
+
+backup.pack({
+  format: 'zip',
+  src: '*.couch',
+  cwd: __dirname + '/data/',
+  dest: dest
+}, function (err, path, bytes) {
+  assert.ifError(err)
+  assert.equal(bytes, 246)
+  assert.equal(path, dest)
+  console.log('#pack() OK')
+  
+  backup.upload(s3, path, bytes, function (err, res) {
+    assert.ifError(err)
+    assert.equal(res.statusCode, 200)
+    
+    fs.open(path, 'r', function (err) {
+      assert.equal(err.code, 'ENOENT')
+      console.log('#upload() OK')
+      
+      setTimeout(function () {
+        backup.clean(s3, '1s', function (err, data) {
+          assert.ifError(err)
+          assert(Array.isArray(data))
+          assert.equal(data.length, 1)
+          console.log('#clean() OK')
+        })
+      }, 2000)
+    })
+  })
+})
